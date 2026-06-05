a5799b2db81c6000ef62ccc488ff8f40a657282c
Pei Lin <a0936856077@gmail.com>
2026-06-06 01:36:06 +0800

    @Test 產生密碼，請勿覆蓋

--- src/test/java/com/example/HellTrain/HellTrainApplicationTests.java
+++ src/test/java/com/example/HellTrain/HellTrainApplicationTests.java
@@ -2,12 +2,21 @@ package com.example.HellTrain;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootTest
 class HellTrainApplicationTests {
+	private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	void pwdGenerator() { // 改為 void
+		String pwd = "MitochondrionIsThePowerPlantOfBody1234";
+		String encodedPwd = encoder.encode(pwd);
+		System.out.println("加密後的密碼： " + encodedPwd);
 	}
 
 }
