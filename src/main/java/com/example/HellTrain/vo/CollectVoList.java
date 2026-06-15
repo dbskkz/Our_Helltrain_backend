b13b36fb7425ca9cc0862a35bd2cce11575299c7
dbskkz <dbskkz1012>
2026-06-15 21:23:39 +0800

    collect回傳資料修改，postman測試通過

--- src/main/java/com/example/HellTrain/vo/CollectVoList.java
+++ src/main/java/com/example/HellTrain/vo/CollectVoList.java
@@ -9,14 +9,22 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class CollectVoList {
 
 	private String productName;
-
-	private String location;
-
+	
 	private String imgPath;
+
+	private String decription;
+	
+	private String condition;
 
 	private int price;
 
 	private String sellerName;
+	
+	private String sellerImg;
+	
+	private String school;
+	
+	private List<String> location;
 	
 	private int collectId;
 
@@ -28,20 +36,28 @@ public class CollectVoList {
 		this.productName = productName;
 	}
 
-	public String getLocation() {
-		return location;
-	}
-
-	public void setLocation(String location) {
-		this.location = location;
-	}
-
 	public String getImgPath() {
 		return imgPath;
 	}
 
 	public void setImgPath(String imgPath) {
 		this.imgPath = imgPath;
+	}
+
+	public String getDecription() {
+		return decription;
+	}
+
+	public void setDecription(String decription) {
+		this.decription = decription;
+	}
+
+	public String getCondition() {
+		return condition;
+	}
+
+	public void setCondition(String condition) {
+		this.condition = condition;
 	}
 
 	public int getPrice() {
@@ -60,6 +76,31 @@ public class CollectVoList {
 		this.sellerName = sellerName;
 	}
 
+	public String getSellerImg() {
+		return sellerImg;
+	}
+
+	public void setSellerImg(String sellerImg) {
+		this.sellerImg = sellerImg;
+	}
+
+	public String getSchool() {
+		return school;
+	}
+
+	public void setSchool(String school) {
+		this.school = school;
+	}
+
+
+	public List<String> getLocation() {
+		return location;
+	}
+
+	public void setLocation(List<String> location) {
+		this.location = location;
+	}
+
 	public int getCollectId() {
 		return collectId;
 	}
@@ -71,12 +112,16 @@ public class CollectVoList {
 	public static CollectVoList from(Object[] row) {
 		CollectVoList vo = new CollectVoList();
 		vo.setProductName((String) row[0]);
-		vo.setLocation((String) row[1]);
-		List<String> imgList = parseJsonList((String) row[2]);
+		List<String> imgList = parseJsonList((String) row[1]);
 		vo.setImgPath(imgList.isEmpty() ? null : imgList.get(0));
-		vo.setPrice((int) row[3]);
-		vo.setSellerName((String) row[4]);
-		vo.setCollectId((int) row[5]);
+		vo.setDecription((String) row[2]);
+		vo.setCondition((String) row[3]);
+		vo.setPrice((int) row[4]);
+		vo.setSellerName((String) row[5]);
+		vo.setSellerImg((String)row[6]);
+		vo.setSchool((String) row[7]);
+		vo.setLocation(parseJsonList((String) row[8]));	
+		vo.setCollectId((int) row[9]);
 		return vo;
 	}
 
